b0152cf9cfaa66e2a344dee661ff108d421e9bf1
xuxuantai <644319731@qq.com>
2019-07-09 09:10:44 +0800

    111

---
+++ src/LeetCode/三数之和.java
@@ -0,0 +1,13 @@
+package LeetCode;
+
+/**
+ * Created by LaoP on 2019/7/9.
+ */
+public class 三数之和 {
+    /**
+     * a + b + c = 0
+     * 先排序
+     * 确定a, b和c为双指针,向前向后扫描找出符合的
+     */
+
+}
--- src/剑指offer/把数组排成最小的数.java
+++ src/剑指offer/把数组排成最小的数.java
@@ -6,7 +6,24 @@ package 剑指offer;
 public class 把数组排成最小的数 {
     public class Solution {
         public String PrintMinNumber(int [] numbers) {
+            StringBuilder result = new StringBuilder();
 
+            for (int i = 0; i < numbers.length; i++) {
+                for (int j = i + 1; j < numbers.length; j++) {
+                    int a = Integer.valueOf("" + numbers[i] + numbers[j]);
+                    int b = Integer.valueOf("" + numbers[j] + numbers[i]);
+                    if(a > b){
+                        int temp = numbers[j];
+                        numbers[j] = numbers[i];
+                        numbers[i] = temp;
+                    }
+                }
+            }
+
+            for (int i = 0; i < numbers.length; i++) {
+                result.append(numbers[i]);
+            }
+            return result.toString();
 
         }
     }
